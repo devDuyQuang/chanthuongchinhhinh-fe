dd6b983aa5d5078a06011f55904112c6ab81f22d
Tran Trung Phuoc <trantrungphuoc7@gmail.com>
2026-05-09 19:30:17 +0700

    fix meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,50 +1,3 @@
-// import type { Metadata } from "next";
-// // import "./globals.css";
-// import 'swiper/css'
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-// import 'swiper/css/grid';
-// import 'swiper/css/thumbs';
-// import 'swiper/css/free-mode';
-// import 'swiper/css/effect-fade';
-// import 'lightgallery/css/lightgallery.css';
-// import 'lightgallery/css/lg-zoom.css';
-// import 'lightgallery/css/lg-thumbnail.css';
-// import "../../public/assets/css/style.css"
-// import ScrolltoTop from "@/component/ScrolltoTop";
-// import HeaderWrapper from "@/component/HeaderWrapper";
-// import FooterWrapper from "@/component/FooterWrapper";
-
-// export const metadata: Metadata = {
-//   title: "ClinicMaster - Health & Medical NextJs Template",
-//   description: "ClinicMaster is a clean and modern Health & Medical Next.js template. Ideal for clinics, hospitals, and healthcare providers, with responsive design, booking system, and customizable components.",
-//   robots: {
-//     index: false,
-//     follow: false,
-//   },
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="vi" data-theme-color="skin-1">
-//       <head>
-//         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>      
-//       </head>
-//       <body>
-//         <HeaderWrapper />
-//         {children}
-//         <FooterWrapper />
-//         <ScrolltoTop />
-//       </body>
-//     </html>
-//   );
-// }
-
-
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 
@@ -75,13 +28,13 @@ import { normalizeImageUrl } from "@/lib/normalizeImageUrl";
 import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
-  title: "ClinicMaster - Health & Medical NextJs Template",
+  title: "DrDuongOrtho - Bác sĩ Dương Chuyên khoa Chấn thương Chỉnh hình",
   description:
-    "ClinicMaster is a clean and modern Health & Medical Next.js template. Ideal for clinics, hospitals, and healthcare providers, with responsive design, booking system, and customizable components.",
-  robots: {
-    index: false,
-    follow: false,
-  },
+    "DrDuongOrtho - Bác sĩ Dương Chuyên khoa Chấn thương Chỉnh hình chuyên khám và điều trị các bệnh lý cơ xương khớp, chấn thương thể thao, đau lưng, thoái hóa khớp và phục hồi vận động với giải pháp an toàn, hiệu quả và tận tâm cho từng bệnh nhân.",
+  // robots: {
+  //   index: false,
+  //   follow: false,
+  // },
 };
 
 async function getSettings() {
