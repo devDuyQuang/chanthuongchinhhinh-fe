b1f6e575702121780e5a557fcf5ed08f850cd5cd
Vo Duy Quang <voquagduy28@gmail.com>
2026-05-10 20:44:26 +0700

    add contact api

--- src/constant/useEmailService.ts
+++ src/constant/useEmailService.ts
@@ -13,7 +13,7 @@ export const useEmailService = () => {
 
       const API_BASE = (
         process.env.NEXT_PUBLIC_API_BASE_URL ||
-        `${(process.env.NEXT_PUBLIC_BASE_URL || "").replace(/\/$/, "")}/api`
+        "http://api.localhost:8000"
       ).replace(/\/$/, "");
 
       const response = await fetch(`${API_BASE}/contact`, {
