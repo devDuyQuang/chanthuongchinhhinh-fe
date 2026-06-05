97b977f810706f406e8fde8ea3ca39fda4cb1233
Tran Trung Phuoc <trantrungphuoc7@gmail.com>
2026-06-05 19:08:17 +0700

    đóng chân trang cũ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ import ScrolltoTop from "@/component/ScrolltoTop";
 import HeaderWrapper from "@/component/HeaderWrapper";
 import FooterWrapper from "@/component/FooterWrapper";
 import FloatingButtons from "@/component/FloatingButtons";
-import FloatingPhone from "@/component/FloatingPhone";
 import { normalizeImageUrl } from "@/lib/normalizeImageUrl";
 import { Toaster } from "react-hot-toast";
 
@@ -89,7 +88,6 @@ export default async function RootLayout({
         <FooterWrapper />
         <ScrolltoTop />
         <FloatingButtons />
-        <FloatingPhone />
         <Toaster position="top-right" />
         {GA_ID && (
           <>
