6f69e8b3f3afa4e6ada4fefc278b847384e0568d
Tran Trung Phuoc <trantrungphuoc7@gmail.com>
2026-06-29 23:35:21 +0700

    301 link

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/noi-soi-khop-goi",
+        destination: "/dich-vu/noi-soi-khop-goi",
+        permanent: true, // 301
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
